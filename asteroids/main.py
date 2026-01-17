313171ef7708ba582a7bd0e79e1fae4251fd22b5
ahedrick <ahedrick4@gmail.com>
2026-01-16 16:10:39 -0800

    Implemented base version of asteroid game with future todos labeled in README

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,11 @@
-import pygame
+import pygame, sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from circleshape import CircleShape
+from shot import Shot
 
 def main():
     pygame.init()
@@ -8,6 +13,23 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    new_clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
+
+    player_character = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
@@ -17,7 +39,25 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+        
+        for object in asteroids:
+            if object.collides_with(player_character):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if shot.collides_with(object):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    object.split()
+        
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = new_clock.tick(60) / 1000
+
+
         
 
 if __name__ == "__main__":
